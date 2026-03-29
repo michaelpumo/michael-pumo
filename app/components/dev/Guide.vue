339981f691b6934f489268e67add98dc942bc8dc
Michael Pumo <hello@michaelpumo.com>
2026-03-29 21:20:37 +0100

    refactor: simplify Guide component structure and update class names

--- app/components/dev/Guide.vue
+++ app/components/dev/Guide.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid-guide">
+
     <input
       :class="[
         'peer fixed z-50 size-12 rounded-full bottom-5 right-5',
@@ -10,16 +10,14 @@
       type="checkbox"
     >
 
-    <div
-      class="wrapper ease-smooth fixed inset-0 z-40 w-full h-full transition-opacity duration-200 pointer-events-none opacity-0 peer-checked:opacity-100"
-    >
-      <div class="grid gap-x-(--app-inner-gutter) grid-cols-2 md:grid-cols-12 h-screen">
+    <div class="wrapper ease-smooth fixed inset-0 z-40 size-full transition-opacity duration-200 pointer-events-none opacity-0 peer-checked:opacity-100">
+      <div class="grid gap-x-(--app-inner-gutter) grid-cols-2 md:grid-cols-12 size-full">
         <div
           v-for="i in 12"
           :key="i"
-          class="guide-col bg-[blue]/20"
+          class="bg-accent/20"
         />
       </div>
     </div>
-  </div>
+
 </template>
