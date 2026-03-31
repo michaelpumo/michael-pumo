9aa077ae1825316a82e5c2c3ea050b678aa54000
Michael Pumo <hello@michaelpumo.com>
2026-03-31 17:41:50 +0100

    feat: refactor link handling in Text component for improved rendering and semantics

--- app/components/storyblok/Text.vue
+++ app/components/storyblok/Text.vue
@@ -1,30 +1,54 @@
 <script lang="ts" setup>
 import type { StoryblokRichtext } from '#storyblok-types'
-import type { StoryblokRichTextNode } from '@storyblok/vue'
+import type { BlockAttributes } from '@storyblok/richtext'
+import type { Component, VNode } from 'vue'
 import { NuxtLink } from '#components'
-
-const resolvers = {
-  [MarkTypes.LINK]: (node: StoryblokRichTextNode<VNode>) => {
-    const isEmail = node.attrs?.linktype === 'email'
-
-    return h(NuxtLink, {
-      to: isEmail ? `mailto:${node.attrs?.href}` : storyblokSlug(node.attrs?.href),
-      target: isEmail ? '_blank' : node.attrs?.target,
-    }, { default: () => node.text ?? '' })
-  },
-}
+import { Mark } from '@tiptap/core'
 
 interface Props {
-  html?: StoryblokRichtext | undefined
+  html?: StoryblokRichtext
 }
 
 const { html } = defineProps<Props>()
+
+const CustomLink = Mark.create({
+  name: 'link',
+  renderHTML({ HTMLAttributes }) {
+    const isEmail = HTMLAttributes.linktype === 'email'
+    const isStory = HTMLAttributes.linktype === 'story'
+
+    const attrs = {
+      to: isEmail ? `mailto:${HTMLAttributes.href}` : isStory ? storyblokSlug(HTMLAttributes.href) : HTMLAttributes.href,
+      target: HTMLAttributes.target || undefined,
+    }
+
+    return [asTag(NuxtLink), attrs, 0]
+  },
+})
+
+type VNodeResult = VNode | VNode[]
+
+const renderFn = (tag: string | Component, attrs: BlockAttributes, children?: VNodeResult): VNodeResult => {
+  if (typeof tag !== 'string' && children != null) {
+    return h(tag, attrs, { default: () => Array.isArray(children) ? children : [children] })
+  }
+
+  return h(tag as string, attrs, children)
+}
+
+const { render } = useStoryblokRichText({
+  renderFn,
+  tiptapExtensions: {
+    link: CustomLink,
+  },
+})
+
+const richText = computed(() => html ? render(html) : null)
 </script>
 
 <template>
-  <StoryblokRichText
-    v-if="html"
-    :doc="html"
-    :resolvers="resolvers"
+  <component
+    :is="() => richText"
+    v-if="richText"
   />
 </template>
